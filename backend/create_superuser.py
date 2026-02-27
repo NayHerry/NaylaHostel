48d6893c123419915887935c058e9e127deb6ccc
NayHerry <Nailakherrynaila@gmail.com>
2026-02-27 15:00:39 +0300

    lets push together with nayla

---
+++ backend/create_superuser.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "nayla")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "1234")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "nayla@example.com")
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS("Superuser created with saved changes both lines"))
